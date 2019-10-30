e376895a36b2290cfbc3bb03c0f66fe5835f849c
Geraldine Vanderkelen <g.vanderkelen@gmail.com>
2019-10-30 15:45:51 +0100

    amazing restaurant website

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,12 @@
 Rails.application.routes.draw do
+  get 'reviews/index'
+  get 'reviews/new'
+  get 'restaurants/index'
+  get 'restaurants/new'
+  get 'restaurants/show'
+  get 'restaurants/create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  resources :restaurants, only: [:index, :new, :create, :show] do
+    resources :reviews, only: [:new, :create]
+  end
 end
